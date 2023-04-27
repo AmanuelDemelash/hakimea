5927cd761c219b1bbfa84f378184daae0590cb97
Amanuel Demelash <amanueldemelash12@gmail.com>
2023-04-27 09:58:47 -0700

    pharmacy card

--- lib/screens/user/user/pharmacy/widget/pharmacy_card.dart
+++ lib/screens/user/user/pharmacy/widget/pharmacy_card.dart
@@ -38,112 +38,136 @@ class pharmacy_card extends StatelessWidget {
     return Container(
         width: Get.width,
         margin: const EdgeInsets.only(left: 10, right: 10, bottom: 9),
+        padding: const EdgeInsets.all(10),
         decoration: const BoxDecoration(
             color: Colors.white,
             borderRadius: BorderRadius.all(Radius.circular(15))),
-        child: Row(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            //pharmacy pic
-            ClipRRect(
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(15),
-                bottomLeft: Radius.circular(15),
-              ),
-              child: CachedNetworkImage(
-                imageUrl: image.toString(),
-                width: 100,
-                height: 140,
-                placeholder: (context, url) => const Icon(Icons.image),
-                errorWidget: (context, url, error) => const Icon(Icons.error),
-                fit: BoxFit.cover,
-              ),
-            ),
-            const SizedBox(
-              width: 5,
-            ),
-            Expanded(
-              child: Container(
-                padding: const EdgeInsets.all(10),
-                child: ListTile(
-                  contentPadding: const EdgeInsets.all(2),
-                  title: Text(name.toString()),
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(location.toString()),
-                      const SizedBox(
-                        height: 5,
-                      ),
-
-                      RatingBar.builder(
-                        initialRating: rate.toDouble(),
-                        minRating: 1,
-                        direction: Axis.horizontal,
-                        allowHalfRating: true,
-                        itemCount: 5,
-                        itemSize: 11,
-                        itemPadding:
-                            const EdgeInsets.symmetric(horizontal: 1.0),
-                        itemBuilder: (context, _) => const Icon(
-                          Icons.star,
-                          color: Constants.secondcolor,
-                        ),
-                        onRatingUpdate: (rating) {},
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      Row(
-                        children: [
-                          const FaIcon(
-                            FontAwesomeIcons.clock,
-                            size: 13,
-                          ),
-                          const SizedBox(
-                            width: 10,
-                          ),
-                          Text("$open_time-$close_time")
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      // owner info
-                      Row(
-                        children: [
-                          CircleAvatar(
-                            radius: 11,
-                            backgroundColor: Constants.primcolor,
-                            backgroundImage: NetworkImage(owner_image),
-                          ),
-                          const SizedBox(
-                            width: 10,
-                          ),
-                          Text(owner_name)
-                        ],
-                      )
-                    ],
+            Row(
+              children: [
+                //pharmacy pic
+                ClipRRect(
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(15),
                   ),
-                  trailing: GestureDetector(
-                    onTap: () async {
-                      launch("tel:$phone_number");
-                    },
-                    child: Container(
-                      width: 35,
-                      height: 35,
-                      decoration: const BoxDecoration(
-                          color: Constants.primcolor, shape: BoxShape.circle),
-                      child: const Center(
-                        child: FaIcon(
-                          FontAwesomeIcons.phone,
-                          color: Colors.white,
-                          size: 18,
+                  child: CachedNetworkImage(
+                    imageUrl: image.toString(),
+                    width: 60,
+                    height: 60,
+                    placeholder: (context, url) => const Icon(Icons.image),
+                    errorWidget: (context, url, error) =>
+                        const Icon(Icons.error),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                const SizedBox(
+                  width: 5,
+                ),
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.all(10),
+                    child: ListTile(
+                      contentPadding: const EdgeInsets.all(2),
+                      title: Text(name.toString()),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(location.toString()),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          RatingBar.builder(
+                            initialRating: rate.toDouble(),
+                            minRating: 1,
+                            direction: Axis.horizontal,
+                            allowHalfRating: true,
+                            itemCount: 5,
+                            itemSize: 11,
+                            itemPadding:
+                                const EdgeInsets.symmetric(horizontal: 1.0),
+                            itemBuilder: (context, _) => const Icon(
+                              Icons.star,
+                              color: Constants.secondcolor,
+                            ),
+                            onRatingUpdate: (rating) {},
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                        ],
+                      ),
+                      trailing: GestureDetector(
+                        onTap: () async {
+                          launch("tel:$phone_number");
+                        },
+                        child: Container(
+                          width: 35,
+                          height: 35,
+                          decoration: const BoxDecoration(
+                              color: Constants.primcolor,
+                              shape: BoxShape.circle),
+                          child: const Center(
+                            child: FaIcon(
+                              FontAwesomeIcons.phone,
+                              color: Colors.white,
+                              size: 18,
+                            ),
+                          ),
                         ),
                       ),
                     ),
                   ),
+                )
+              ],
+            ),
+
+            //time
+            const Text(
+              "Opening time",
+              style: TextStyle(color: Colors.black45),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            Row(
+              children: [
+                const FaIcon(
+                  FontAwesomeIcons.clock,
+                  size: 13,
                 ),
-              ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Text("$open_time-$close_time")
+              ],
+            ),
+            // owner
+            const SizedBox(
+              height: 5,
+            ),
+            const Text(
+              "Owner",
+              style: TextStyle(color: Colors.black45),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+
+            Row(
+              children: [
+                CircleAvatar(
+                  radius: 11,
+                  backgroundColor: Constants.primcolor,
+                  backgroundImage: NetworkImage(owner_image),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Text(owner_name)
+              ],
             )
           ],
         ));
