6b63d814bf1e27ed6ec0e285e30e6dd00ac539a3
Amanuel Demelash <amanueldemelash12@gmail.com>
2023-05-19 12:59:08 -0700

    prescription

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,29 +13,6 @@ import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:hakimea/controllers/user_controllers/logincontroller.dart';
 import 'package:hakimea/screens/auth/login.dart';
 
-void main() {
-  testWidgets("login test", (widgetTester)async{
-    Get.put(LoginController());
-  await widgetTester.pumpWidget(GetMaterialApp(
-      home: Login(),
-    ),
-  );
+void main(){
 
-    final ctr=find.text("Continue as guest");
-
-    expect(ctr, findsNothing);
-
-
-    // await  widgetTester.enterText(email_textfiled, "amanueldemelash@gmail.com");
-    //  await widgetTester.enterText(password_textfiled, "aman1234");
-    //  //await widgetTester.tap(login_button);
-    //  await widgetTester.pump();
-
-    // cheek output
-    // expect(find.text("amanueldemelash@gmail.com"), findsOneWidget);
-    // expect(find.text("aman1234"), findsOneWidget);
-    
-
-
-  });
 }
